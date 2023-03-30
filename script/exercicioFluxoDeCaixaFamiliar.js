f7b8a37927364415a558f213a58349a53ec991a3
Thiagomatheus12 <thi_ago5@hotmail.com>
2023-03-30 09:57:28 -0300

    finalização dos exercicios de javaScript

---
+++ script/exercicioFluxoDeCaixaFamiliar.js
@@ -0,0 +1,42 @@
+/**
+ * Sistema de gastos familiar
+ * 
+ * Crie um objeto que possuirá 2 propriedades, ambas do tipo array:
+ * receitas[]
+ * despesas[]
+ * 
+ * agora, crie uma função que irá calcular o total de receitas e despesas e irá mostrar uma mensagem se a família está com saldo positivo ou negativo, seguido do valor do seu saldo
+ */
+
+let family = {
+  incomes: [2500, 3200, 250.43, 360.45],
+  expenses: [320.34, 128.45, 176.87, 1450.00, 2999]
+}
+
+function sum(array) {
+  let total = 0;
+
+  for(let value of array) {
+    total += value
+  }
+
+  return total
+}
+
+function calculateBalance() {
+  const calculateIncomes = sum(family.incomes)
+  const calculateExpenses = sum(family.expenses)
+
+  const total = calculateIncomes - calculateExpenses
+  const itsOK = total >= 0
+
+  let balanceText = 'negativo'
+  if (itsOK) {
+    balanceText = 'positivo'
+  }
+
+  console.log(`Seu saldo é ${balanceText}: ${total.toFixed(2)}R$`)
+}
+
+
+calculateBalance()
